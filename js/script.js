a0c92d60b694376f5e24347ef3da8998ecd6d92e
Tonio Adams <avcx12@gmail.com>
2023-03-13 21:38:03 -0600

    Update script.js
    Created new functions to increase readability.

--- js/script.js
+++ js/script.js
@@ -38,30 +38,45 @@ function showPage(list, page) {
     element with specific information showing.
 */
    function createStudent(studentData) {
+
       const listItem = document.createElement('li');
       listItem.classList.add('student-item', 'cf');
 
-      const detailsDiv = document.createElement('div');
-      detailsDiv.classList.add('student-details');
+      const detailsDiv = createDiv('student-details', studentData);
+
       listItem.appendChild(detailsDiv);
 
-      detailsDiv.insertAdjacentHTML('afterbegin', `
-         <img class="avatar" src="${studentData.picture.large}" alt="Profile Picture of ${studentData.name.first}">
-         <h3>${studentData.name.first} ${studentData.name.last}</h3>
-         <span class="email">${studentData.email}</span>
-      `)
+      const dateJoinedDiv = createDiv('joined-details', studentData);
 
-      const dateJoinedDiv = document.createElement('div');
-      dateJoinedDiv.classList.add('joined-details');
       listItem.appendChild(dateJoinedDiv);
-
-      dateJoinedDiv.insertAdjacentHTML('afterbegin', `
-         <span class="date">${studentData.registered.date}</span>
-      `);
-
 
       ul.appendChild(listItem); // Prints the student's info card to the page.
 
+   }
+
+   function createDiv(divClass, studentData) {
+      const div = document.createElement('div')
+      div.classList.add(divClass);
+
+      const divInfo = {
+            studentDetails: () => {
+               return `
+               <img class="avatar" src="${studentData['picture']['large']}" alt="Profile Picture of ${studentData['name']['first']}">
+               <h3>${studentData['name']['first']} ${studentData['name']['last']}</h3>
+               <span class="email">${studentData['email']}</span>
+            `},
+            joinedDetails: () => {
+               return `
+               <span class="date">${studentData['registered']['date']}</span>
+            `}
+            };
+      if (divClass === "student-details") {
+         div.insertAdjacentHTML("afterbegin", divInfo.studentDetails());
+
+      } else if (divClass === "joined-details") {
+         div.insertAdjacentHTML("afterbegin", divInfo.joinedDetails());
+      }
+      return div;
    }
 
    // Runs this code block for each "student" object of the "list" array
@@ -87,19 +102,23 @@ This function will create and insert/append the elements needed for the paginati
 
 function addPagination(list) {
 
-   if (currentList.length < 1) {
-      return false;
-   }
+// Calculates the minimum number of pages need to display all the contents of the list.
   const numOfPages = Math.ceil(list.length / 9);
+
+// Claars out the buttons
   pageLinks.innerHTML = '';
 
+// Adds button to the page for each nesecessary page needed for the provided list.
   for (let i = 1; i <= numOfPages; i++) {
+
+// If the button's number is the number of the current page set that button to the active button
    if (i === +currentPage) {
       pageLinks.insertAdjacentHTML('beforeend', `
          <li>
             <button type="button" class="active">${i}</button>
          </li>
       `)
+// Else just insert a regular button.
    } else {
       pageLinks.insertAdjacentHTML('beforeend', `
       <li>
@@ -138,28 +157,37 @@ const searchButton = document.querySelector('label > button');
 const searchField = document.querySelector('#search');
 
 /*
-   `search` Function takes the "data" array and uses the `filter` method to filter
+   `search` Function takes the "data" array and uses the `filter` method to filter if the
+   first or last name of a student in the list matches the text field.
 */
 const search = (_e) => {
 
    const userSearch = searchField.value;
 
    const searchResults = data.filter( student => {
+
       const firstName = student.name.first;
       const lastName = student.name.last;
 
       return firstName.toLowerCase().startsWith(userSearch.toLowerCase()) || lastName.toLowerCase().startsWith(userSearch.toLowerCase());
    })
+// If no results are found a messages is printed to the page.
    if (searchResults.length < 1) {
       return ul.innerHTML = `<h1>404 No Results Found for "${userSearch}"</h1>`
    }
+
+// Updates the global 'current' variables to retain page functionalty after a search is initiated.
    currentList = searchResults;
    currentPage = 1;
 
-   showPage(searchResults, currentPage);
+// Prints search results to the screen.
+   showPage(searchResults, 1);
 }
 
+// Event listeners for when a search action is intiated.
    searchButton.addEventListener('click', search)
 
    searchField.addEventListener('input', search)
+
+// Initalizes Program...
 showPage(currentList, currentPage);
